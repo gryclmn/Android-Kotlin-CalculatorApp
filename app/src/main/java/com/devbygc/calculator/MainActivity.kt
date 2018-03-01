da7d192d795990af65adfeeef4cd98d00d96ed1b
Gary Coleman <gryclmn@hotmail.com>
2018-02-28 16:43:05 -0800

    More learning with the Calculator App

--- app/src/main/java/com/devbygc/calculator/MainActivity.kt
+++ app/src/main/java/com/devbygc/calculator/MainActivity.kt
@@ -7,14 +7,18 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+//private val TAG: String = "MainActivity"
+private const val STATE_PENDING_OPERATION = "PendingOperation"
+private const val STATE_OPERAND1 = "Operand1"
+private const val STATE_OPERAND1_STORED = "Operand1_Stored"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var result: EditText
     private lateinit var newNumber: EditText
-    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation)}
+    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
 
     // Variables to hold the operands and operator
     private var operand1: Double? = null
-    private var operand2: Double = 0.0
     private var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,8 +48,8 @@ class MainActivity : AppCompatActivity() {
         val buttonMinus: Button = findViewById(R.id.buttonMinus)
         val buttonPlus: Button = findViewById(R.id.buttonPlus)
 
-        val listener = View.OnClickListener { v ->
-            val b = v as Button
+        val listener = View.OnClickListener { thisView ->
+            val b = thisView as Button
             newNumber.append(b.text)
         }
 
@@ -61,15 +65,72 @@ class MainActivity : AppCompatActivity() {
         button9.setOnClickListener(listener)
         buttonDot.setOnClickListener(listener)
 
-        val opListener = View.OnClickListener { v ->
-            val op = (v as Button).text.toString()
-            val value = newNumber.text.toString()
-            if (value.isNotEmpty()) {
-//                performOperation(value, op)
+        val opListener = View.OnClickListener { thisView ->
+            val operation = (thisView as Button).text.toString()
+            try {
+                val value = newNumber.text.toString().toDouble()
+                performOperation(value, operation)
+            } catch (e: NumberFormatException) {
+                newNumber.setText("")
             }
-            pendingOperation = op
+
+            pendingOperation = operation
             displayOperation.text = pendingOperation
         }
 
+        buttonEquals.setOnClickListener(opListener)
+        buttonDivide.setOnClickListener(opListener)
+        buttonMultiply.setOnClickListener(opListener)
+        buttonMinus.setOnClickListener(opListener)
+        buttonPlus.setOnClickListener(opListener)
+
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        // Note it is risky to override this method with "Bundle" instead of "Bundle?"
+        super.onRestoreInstanceState(savedInstanceState)
+        operand1 = if (savedInstanceState.getBoolean(STATE_OPERAND1_STORED, false)) {
+            savedInstanceState?.getDouble(STATE_OPERAND1)
+        } else {
+            null
+        }
+
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION)
+        displayOperation.text = pendingOperation
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        // Note it is risky to override this method with "Bundle" instead of "Bundle?"
+        super.onSaveInstanceState(outState)
+        if (operand1 != null) {
+            outState.putDouble(STATE_OPERAND1, operand1!!)
+            outState.putBoolean(STATE_OPERAND1_STORED, true)
+        }
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation)
+    }
+
+    private fun performOperation(value: Double, operation: String) {
+        if (operand1 == null) {
+            operand1 = value
+        } else {
+            if (pendingOperation == "=") {
+                pendingOperation = operation
+            }
+
+            when (pendingOperation) {
+                "=" -> operand1 = value
+                "/" -> operand1 = if (value == 0.0) {
+                    Double.NaN // handle attempt to divide by zero
+                } else {
+                    operand1!! / value
+                }
+                "*" -> operand1 = operand1!! * value
+                "-" -> operand1 = operand1!! - value
+                "+" -> operand1 = operand1!! + value
+            }
+        }
+        result.setText(operand1.toString())
+        newNumber.setText("")
+    }
+
 }
